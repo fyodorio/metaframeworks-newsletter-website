f9bdc47d5bf0dac11cc29c906e857d618e5ff378
fyodorio <alinetofyodor@gmail.com>
2024-12-28 22:47:52 +0500

    refactor: turn blog to newsletter archive

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Metaframeworks Weekly';
+export const SITE_DESCRIPTION =
+  'The most important news and updates in the world of metaframeworks';
