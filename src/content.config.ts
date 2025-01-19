6bafca778d236e48f02e966aeb093d0d1649a041
fyodorio <alinetofyodor@gmail.com>
2025-01-19 15:17:47 +0500

    feat: add comparison page

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,14 +1,11 @@
-import { glob } from 'astro/loaders';
+import { glob, file } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const archive = defineCollection({
-  // Load Markdown and MDX files in the `src/content/archive/` directory.
   loader: glob({ base: './src/content/archive', pattern: '**/*.{md,mdx}' }),
-  // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    // Transform string to Date object
     pubDate: z.coerce.date().optional(),
     updatedDate: z.coerce.date().optional(),
     heroImage: z.string().optional(),
@@ -16,4 +13,58 @@ const archive = defineCollection({
   })
 });
 
-export const collections = { archive };
+const pages = defineCollection({
+  loader: glob({ base: './src/content/pages', pattern: '**/*.{md,mdx}' }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date().optional(),
+    updatedDate: z.date().optional(),
+    heroImage: z.string().optional(),
+    published: z.boolean().optional()
+  })
+});
+
+const metaframeworks = defineCollection({
+  loader: file('src/data/metaframeworks.json'),
+  schema: z.object({
+    id: z.string(),
+    name: z.string(),
+    description: z.string(),
+    officialSiteUrl: z.string(),
+    gitHubUrl: z.string(),
+    runtime: z.array(
+      z.enum(['Node.js', 'Deno', 'Bun', 'Serverless Functions', 'Edge compute'])
+    ),
+    bundler: z.array(
+      z.enum([
+        'Vite',
+        'Webpack',
+        'Turbopack',
+        'Rspack',
+        'Rollup',
+        'Parcel',
+        'esbuild',
+        'SWC'
+      ])
+    ),
+    frontendFramework: z.array(
+      z.enum(['React', 'Vue', 'Angular', 'Svelte', 'Solid', 'Custom'])
+    ),
+    templatingLanguage: z.array(z.enum(['JSX', 'TSX', 'DSL'])),
+    routingType: z.array(z.enum(['file-based', 'programmatic'])),
+    serverFramework: z.enum(['Express', 'Nitro', 'Vinxi', 'Custom']),
+    renderingModes: z.array(z.enum(['SPA', 'SSR', 'SSG', 'Hybrid'])),
+    enterpriseReadiness: z.enum(['High', 'Medium', 'Low']),
+    complexityLevel: z.enum(['High', 'Medium', 'Low']),
+    scalabilityLevel: z.enum(['High', 'Medium', 'Low']),
+    bundleSizeLevel: z.enum(['High', 'Medium', 'Low']),
+    batteriesIncludedLevel: z.enum(['High', 'Medium', 'Low'])
+  })
+});
+
+export const collections = {
+  archive,
+  pages,
+  metaframeworks
+};
