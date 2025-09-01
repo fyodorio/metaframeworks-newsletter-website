e50f7b6addd06e9cebb99618f39552f1f185e8c7
fyodorio <alinetofyodor@gmail.com>
2025-09-01 15:47:17 +0500

    content: rebranding

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Metaframeworks Weekly';
+export const SITE_TITLE = 'Metaframeworks Records';
 export const SITE_DESCRIPTION =
   'The most important news and updates from the world of metaframeworks';
