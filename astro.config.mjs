e1fefbf0a95f6a8a859e28ab19495f26c61043a8
fyodorio <alinetofyodor@gmail.com>
2025-12-02 13:10:32 +0500

    feat: add search with pagefind
    * update Astro dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import pagefind from 'astro-pagefind';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://metaframe.works',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), pagefind()],
 });
