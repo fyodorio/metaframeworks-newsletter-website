23362c7d9e66be07d4b259b5227e3bd4891bd627
fyodorio <alinetofyodor@gmail.com>
2026-04-11 22:12:40 +0500

    style: add anchors for internal text chapters

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from 'astro/config';
 import pagefind from 'astro-pagefind';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://metaframe.works',
 	integrations: [mdx(), sitemap(), pagefind()],
+	markdown: {
+		rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'append' }]]
+	}
 });
